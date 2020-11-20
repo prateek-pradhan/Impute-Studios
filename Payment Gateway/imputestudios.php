6a8105a8553bde734a45be3dc19ac11386ebe99f
Prateek-2001 <55329333+Prateek-2001@users.noreply.github.com>
2020-11-21 03:37:58 +0530

    Add files via upload

---
+++ Payment Gateway/imputestudios.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Impute Studios</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+    
+    
+        <header>
+            <img src="images/logo-imputestudios.jpeg" alt="Logo for Impute Studios">
+        </header>
+        <nav>
+            <a href="imputestudios.php">HOME</a>
+            <a href="checkout.php">DONATE TO THE GAME</a>
+            <a href="aboutus.html">ABOUT US</a>
+        </nav>
+        <main>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/DDx__QKDyaM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            
+        </main>
+       
+
+        <footer>
+            <p>Website made by Prateek Pradhan</p>
+        </footer>
+    </div>
+
+</body>
+</html>
